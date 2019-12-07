9af46e2896f6a83dbe54e1e3ca15326c0a9f5eb2
abdisudiatmika <sudiatmika.abdi@gmail.com>
2019-12-07 10:42:38 +0700

    tambah data main initial migration

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"go-training-restful/config"
+	"go-training-restful/models"
 	"go-training-restful/routes"
 )
 
 func main() {
 	e := routes.New()
 
-	e.Logger.Fatal(e.Start(":80000"))
+	InitialMigration()
+
+	e.Logger.Fatal(e.Start(":8000"))
+}
+
+func InitialMigration() {
+	var db = config.DB
+	if !db.HasTable(&models.User{}) {
+		db.AutoMigrate(&models.User{})
+	}
 }
