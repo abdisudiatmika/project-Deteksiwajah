8c8f3ccad018aa8eab2397ffcceb15a2ad410df4
abdisudiatmika <sudiatmika.abdi@gmail.com>
2019-12-07 10:56:28 +0700

    tambah data user controler

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,11 +1,15 @@
 package controllers
 
 import (
+	"go-training-restful/config"
 	"go-training-restful/database"
+	"go-training-restful/models"
 	"net/http"
 
 	"github.com/labstack/echo"
 )
+
+var db = config.DB
 
 //GetUsersController function mengambil data user
 func GetUsersController(c echo.Context) error {
@@ -20,4 +24,12 @@ func GetUsersController(c echo.Context) error {
 		"massage": "mendapatkan data",
 		"data":    users,
 	})
+}
+
+//CreateUserController function menambah user
+func CreateUserController(user *models.User) (interface{}, error) {
+	if err := db.Create(&user).Error; err != nil {
+		return nil, err
+	}
+	return user, nil
 }
