6beed43ae35595099b7e804f3b533f6b95459c27
abdisudiatmika <sudiatmika.abdi@gmail.com>
2019-12-07 14:38:45 +0700

    tambah middleware

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	c "go-training-restful/controllers"
+	m "go-training-restful/middleware"
 
 	"github.com/labstack/echo"
 )
@@ -9,7 +10,7 @@ import (
 //New fungsi baru untuk routing
 func New() *echo.Echo {
 	e := echo.New()
-
+	m.LogMiddleware(e)
 	e.GET("users", c.GetUsersController)
 	e.GET("/users/:id", c.GetUserController)
 	e.POST("users", c.CreateUserController)
