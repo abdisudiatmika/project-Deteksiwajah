d57bb904b87154e0f67629d86446978aaa2a9c04
abdisudiatmika <sudiatmika.abdi@gmail.com>
2019-12-10 07:00:00 +0700

    mencoba aplikasi baru

--- routes/routes.go
+++ routes/routes.go
@@ -21,11 +21,6 @@ func New() *echo.Echo {
 	e.DELETE("/userseye/:id", c.DeleteeyedbController)
 	e.PUT("/userseye/:id", c.UpdateeyedbController)
 
-	// eUser := e.Group("/users/")
-	// eUser.Use(echoMid.BasicAuth(m.BasicAuth))
-	// eUser.PUT(":id", c.UpdateUserController)
-	// eUser.DELETE(":id", c.DeleteUserController)
-	//e.POST("findpersonal", c.GetFacePersonal)
 	return e
 
 }
